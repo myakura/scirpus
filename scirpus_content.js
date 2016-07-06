cb3cbcc5c429768684d2f004aefee05934f0fae1
Masataka Yakura <masataka.yakura@gmail.com>
2016-07-06 16:41:01 +0900

    use class syntax

--- scirpus_content.js
+++ scirpus_content.js
@@ -1,64 +1,66 @@
 'use strict'
 
-const ampLinkElement = document.querySelector(`link[rel="amphtml"][href]`)
-
-const isAMPPage = () => {
-  const html = document.documentElement
-  return html.hasAttribute('⚡') || html.hasAttribute('amp')
+class ScirpusContent {
+  getAMPLinkElement () {
+    return document.querySelector(`link[rel="amphtml"][href]`)
+  }
+  isAMPPage () {
+    const htmlElement = document.documentElement
+    return htmlElement.hasAttribute('⚡') || htmlElement.hasAttribute('amp')
+  }
+  hasAMPPage () {
+    const ampLinkElement = this.getAMPLinkElement()
+    return !!ampLinkElement
+  }
+  getAMPPageURL () {
+    if (!this.hasAMPPage()) {
+      return null
+    }
+    return this.getAMPLinkElement().href
+  }
+  getAMPCacheURL () {
+    if (!this.hasAMPPage()) {
+      return null
+    }
+    const ampURL = this.getAMPPageURL()
+    const ampCacheURLPrefix = 'https://cdn.ampproject.org/c/'
+    if (ampURL.startsWith(ampCacheURLPrefix)) {
+      return ampURL
+    }
+    // for HTTPS AMP pages, the prefix has additional `s/`
+    const secure = (new URL(ampURL)).protocol === 'https:'
+    return `${ampCacheURLPrefix}${!!secure ? 's/': ''}${ampURL.replace(/https?:\/\//, '')}`
+  }
+  getOriginalPageURL () {
+    const canonicalLinkElement = document.querySelector(`link[rel="canonical"][href]`)
+    if (this.isAMPPage() && !!canonicalLinkElement) {
+      return canonicalLinkElement.href
+    }
+    else {
+      return null
+    }
+  }
+  getPageInfo () {
+    return {
+      pageURL: location.href,
+      hasAMPPage: this.hasAMPPage(),
+      ampPageURL: this.getAMPPageURL(),
+      ampCacheURL: this.getAMPCacheURL(),
+      isAMPPage: this.isAMPPage(),
+      originalPageURL: this.getOriginalPageURL(),
+    }
+  }
 }
 
-const hasAMPPage = () => {
-  return !!ampLinkElement
-}
-
-const getAMPPageURL = () => {
-  if (!hasAMPPage()) {
-    return null
-  }
-  return ampLinkElement.href
-}
-
-const getAMPCacheURL = () => {
-  if (!hasAMPPage()) {
-    return null
-  }
-  const ampURL = getAMPPageURL()
-  const ampCacheURLPrefix = 'https://cdn.ampproject.org/c/'
-  if (ampURL.startsWith(ampCacheURLPrefix)) {
-    return ampURL
-  }
-  // for HTTPS AMP pages, the prefix has additional `s/`
-  const secure = (new URL(ampURL)).protocol === 'https:'
-  return `${ampCacheURLPrefix}${!!secure ? 's/': ''}${ampURL.replace(/https?:\/\//, '')}`
-}
-
-const getOriginalPageURL = () => {
-  const canonicalLinkElement = document.querySelector(`link[rel="canonical"][href]`)
-  if (isAMPPage() && !!canonicalLinkElement) {
-    return canonicalLinkElement.href
-  }
-  else {
-    return null
-  }
-}
-
-const getPageInfo = () => {
-  return {
-    pageURL: location.href,
-    hasAMPPage: hasAMPPage(),
-    ampPageURL: getAMPPageURL(),
-    ampCacheURL: getAMPCacheURL(),
-    isAMPPage: isAMPPage(),
-    originalPageURL: getOriginalPageURL(),
-  }
-}
+// initialize
+const scirpusContent = new ScirpusContent()
 
 // message to background page
-chrome.runtime.sendMessage({name: 'page-info', data: getPageInfo()})
+chrome.runtime.sendMessage({name: 'page-info', data: scirpusContent.getPageInfo()})
 
 // message from background page
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.name === 'get-page-info') {
-    sendResponse({name: 'page-info', data: getPageInfo()})
+    sendResponse({name: 'page-info', data: scirpusContent.getPageInfo()})
   }
 })
