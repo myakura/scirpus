356380cd6c42ae5220ca5459710fc2f659e30730
Masataka Yakura <masataka.yakura@gmail.com>
2016-07-06 18:20:12 +0900

    remove the conditonal that is no longer needed

--- scirpus_background.js
+++ scirpus_background.js
@@ -1,19 +1,17 @@
 'use strict'
 
 const updatePageAction = (message, tabID) => {
-  if (message.name === 'page-info') {
-    if (message.data.hasAMPPage) {
-      chrome.pageAction.setTitle({tabId: tabID, title: 'AMP page found for this page ⚡'})
-      chrome.pageAction.show(tabID)
-    }
-    else if (message.data.isAMPPage) {
-      chrome.pageAction.setTitle({tabId: tabID, title: 'This is an AMP page ⚡'})
-      chrome.pageAction.show(tabID)
-    }
-    else {
-      chrome.pageAction.setTitle({tabId: tabID, title: 'No AMP page found for this page 😫'})
-      chrome.pageAction.hide(tabID)
-    }
+  if (message.data.hasAMPPage) {
+    chrome.pageAction.setTitle({tabId: tabID, title: 'AMP page found for this page ⚡'})
+    chrome.pageAction.show(tabID)
+  }
+  else if (message.data.isAMPPage) {
+    chrome.pageAction.setTitle({tabId: tabID, title: 'This is an AMP page ⚡'})
+    chrome.pageAction.show(tabID)
+  }
+  else {
+    chrome.pageAction.setTitle({tabId: tabID, title: 'No AMP page found for this page 😫'})
+    chrome.pageAction.hide(tabID)
   }
 }
 
